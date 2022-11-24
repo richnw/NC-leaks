ccb64aef865fb141a869fb87fac08e760d9a70e2
andmikpar <andymikeparks@gmail.com>
2022-11-24 14:07:54 +0000

    challenge 1 completed

--- challenges.js
+++ challenges.js
@@ -1,3 +1,36 @@
+const https = require("node:https");
+const fs = require("fs");
+const { parse } = require("node:path");
+
 function getPeople() {
-  return "Rich";
+ 
+  const options = {
+    hostname: "nc-leaks.herokuapp.com",
+    path: "/api/people",
+    method: "GET",
+  }
+
+  const request = https.request(options, (response) => {
+    response.setEncoding('utf8');
+
+    let body = "";
+
+    response.on("data", (packet) => {
+      body += packet;
+    })
+    response.on("end", () => {
+      const parsedBody = JSON.parse(body);
+      const people = parsedBody.people
+      // console.log(people)
+      const northcoders = people.filter((person) => person.job.workplace == 'northcoders')
+      const jsonNorthcoders = JSON.stringify(northcoders);
+      fs.writeFile("northcoders.json", jsonNorthcoders, (err) => {
+        console.log('file saved');
+      })
+    })
+
+    
+  })
+  request.end();
 }
+getPeople();
